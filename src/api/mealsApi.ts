1d4a4c5028353491cfcb3958bac95d0f7b3c4538
Nur Ismailov <nismailovwork@gmail.com>
2026-06-15 09:46:00 +0600

    implemented api logic to fetch meals and show in console

--- src/api/mealsApi.ts
+++ src/api/mealsApi.ts
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import type { MealData, MealItem } from '../types/meal.ts';
 
 const mealEndpoint = axios.create({
   baseURL: 'https://js-31-nurisma-default-rtdb.europe-west1.firebasedatabase.app/',
 });
+
+export const mealsApi = {
+  async getMeals(): Promise<MealItem[]> {
+    const response = await mealEndpoint.get<Record<string, MealData> | null>(
+      '/meals.json'
+    );
+
+    if (!response.data) return [];
+
+    return Object.entries(response.data).map(([id, meal]) => ({
+      id,
+      ...meal,
+    }));
+  }
+};
--- src/pages/MealsPage/MealsPage.tsx
+++ src/pages/MealsPage/MealsPage.tsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+import { mealsApi } from "../../api/mealsApi.ts";
+
 const MealsPage = () => {
+
+
+  const fetchMeals = async () => {
+    try {
+      const data = await mealsApi.getMeals();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    void fetchMeals();
+  }, []);
+
   return (
     <div>
       <h1>Meals Page</h1>
